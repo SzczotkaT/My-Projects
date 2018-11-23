23c07f98b9e018507e80fc8cb756bd16e500a058
SzczotkaT <tomasz.szczotka89@gmail.com>
2018-11-23 14:29:16 +0100

    Task 18.3 Replay Extension

--- src/main/java/com/crud/tasks/domain/BadgesDto.java
+++ src/main/java/com/crud/tasks/domain/BadgesDto.java
@@ -10,18 +10,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class CreatedTrelloCard {
+public class BadgesDto {
 
-    @JsonProperty("id")
-    private String id;
+    @JsonProperty("votes")
+    private int votes;
 
-    @JsonProperty("badges")
-    private BadgesDto badges;
-
-    @JsonProperty("name")
-    private String name;
-
-    @JsonProperty("shortUrl")
-    private String shortUrl;
-
+    @JsonProperty("attachmentsByType")
+    private AttachmentsByType attachment;
 }
